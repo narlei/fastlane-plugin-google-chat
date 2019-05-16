ad472c3f2367a925d3d5e34160088d6ef75adc99
Narlei Américo Moreira <narlei.guitar@gmail.com>
2019-05-16 14:12:22 -0300

    Fix

--- lib/fastlane/plugin/google_chat/actions/google_chat_action.rb
+++ lib/fastlane/plugin/google_chat/actions/google_chat_action.rb
@@ -3,82 +3,82 @@
   require 'net/http'
   require 'uri'
   require 'json'
-
+  
   module Fastlane
     module Actions
       class GoogleChatAction < Action
         def self.run(params)
-
-          uri = URI.parse(options[:webhook])
-
+          
+          uri = URI.parse(params[:webhook])
+          
           header = {'Content-Type': 'application/json'}
           cards = [
-              {
-                  header: {
-                      title: options[:title],
-                      subtitle: options[:description],
-                      imageUrl: 'https://collegeinfogeek.com/wp-content/uploads/2016/08/Google_Calendar_Logo.png'
-                  },
-                  sections: [
-                      {
-                          widgets: [
-                              {
-                                  keyValue: {
-                                      topLabel: options[:section1Title],
-                                      content: options[:section1Description]
-                                  }
-                              }
-                          ]
-                      },
-                      {
-                          widgets: [
-                              {
-                                  buttons: [
-                                      {
-                                          textButton: {
-                                              text: options[:buttonTitle],
-                                              onClick: {
-                                                  openLink: {
-                                                      url: options[:buttonUrl]
-                                                  }
-                                              }
-                                          }
-                                      }
-                                  ]
-                              }
-                          ]
+            {
+              header: {
+                title: params[:title],
+                subtitle: params[:description],
+                imageUrl: 'https://collegeinfogeek.com/wp-content/uploads/2016/08/Google_Calendar_Logo.png'
+              },
+              sections: [
+                {
+                  widgets: [
+                    {
+                      keyValue: {
+                        topLabel: params[:section1Title],
+                        content: params[:section1Description]
                       }
+                    }
                   ]
-              }
+                },
+                {
+                  widgets: [
+                    {
+                      buttons: [
+                        {
+                          textButton: {
+                            text: params[:buttonTitle],
+                            onClick: {
+                              openLink: {
+                                url: params[:buttonUrl]
+                              }
+                            }
+                          }
+                        }
+                      ]
+                    }
+                  ]
+                }
+              ]
+            }
           ]
           # Create the HTTP objects
           http = Net::HTTP.new(uri.host, uri.port)
           request = Net::HTTP::Post.new(uri.request_uri, header)
-          request.body = card.to_json
+          request.body = cards.to_json
           
           # Send the request
           response = http.request(request)
-
+          
           UI.message("The google_chat plugin is working!")
         end
-
+        
         def self.description
           "Send messages to Google Chat"
         end
-
+        
         def self.authors
           ["Narlei Américo Moreira"]
         end
-
+        
         def self.return_value
           # If your method provides a return value, you can describe here what it does
         end
-
+        
         def self.details
           # Optional:
           "Send messages to Google Chat rooms"
         end
-
+        
         def self.available_options
           [
             # FastlaneCore::ConfigItem.new(key: :your_option,
@@ -88,7 +88,7 @@
             #                             type: String)
           ]
         end
-
+        
         def self.is_supported?(platform)
           # Adjust this if your plugin only works for a particular platform (iOS vs. Android, for example)
           # See: https://docs.fastlane.tools/advanced/#control-configuration-by-lane-and-by-platform
@@ -99,3 +99,4 @@
       end
     end
   end
+  
