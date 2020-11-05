c7845d6b8aa4ca41e47be39dde4d2b0b189932eb
Narlei Moreira <narlei.guitar@gmail.com>
2020-11-05 12:38:02 -0300

    Add SSL

--- lib/fastlane/plugin/google_chat/actions/google_chat_action.rb
+++ lib/fastlane/plugin/google_chat/actions/google_chat_action.rb
@@ -51,15 +51,15 @@
           ]}
           # Create the HTTP objects
           http = Net::HTTP.new(uri.host, uri.port)
-          # http.use_ssl = true
-          # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          http.use_ssl = true
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
           request = Net::HTTP::Post.new(uri.request_uri)
           request.body = cards.to_json
           
           # Send the request
           response = http.request(request)
 
-          UI.message("Message sent!")
+          UI.message("Message sent!" + response)
         end
         
         def self.description
